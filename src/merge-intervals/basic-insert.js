f0b6eda452fa3ec34e5cffabedb05b975bd47312
sean mombo <sadfib@hotmail.com>
2020-02-24 15:46:09 -0500

    insert written

--- src/merge-intervals/basic-insert.js
+++ src/merge-intervals/basic-insert.js
@@ -29,6 +29,31 @@ const insert = function(arr, newInterval) {
     //We have arrived at the end of the list, or we've arrived at our insertion location
 
     while (i < arr.length && arr[i].start <= newInterval.end) {
-
+        const curr = arr[i];
+        //Merge any intervals that overlap the interval we are inserting
+            // min the start values so our new interval starts as early as possible
+            newInterval.start = Math.min(curr.start, newInterval.start);
+            // max the end values so it ends as late as possible
+            newInterval.end = Math.max(curr.end, newInterval.end);
+        i++;
     }
+    merged.push(newInterval);
+
+    // add the remaining intervals
+    while(i < arr.length) {
+        merged.push(arr[i]);
+        i++;
+    }
+    return merged; 
 }
+
+const arr = [new Interval(1,3), new Interval(4, 9), new Interval(15, 17), new Interval(19, 30)];
+
+console.log(insert(arr, new Interval(10, 16)));
+
+// [
+//     Interval { start: 1, end: 3 },
+//     Interval { start: 4, end: 9 },
+//     Interval { start: 10, end: 17 },
+//     Interval { start: 19, end: 30 }
+// ]
